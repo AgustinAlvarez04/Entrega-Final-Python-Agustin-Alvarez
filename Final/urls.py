bdbb1ec5cebffa32a7b5a6e980e3863f01292a2d
AgustinAlvarez04 <121886254+AgustinAlvarez04@users.noreply.github.com>
2023-01-23 00:09:22 -0300

    Add files via upload

--- Final/urls.py
+++ Final/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from App.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("App.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls'))
 ]
+
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
