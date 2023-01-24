011e3531c95bd7d3ac968b796939e507e66bebd9
AgustinAlvarez04 <121886254+AgustinAlvarez04@users.noreply.github.com>
2023-01-24 03:53:25 -0300

    Add files via upload

---
+++ App/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+# Create your models here.
+
+class Avatar(models.Model):
+    imagen= models.ImageField(upload_to="avatars")
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.user}"
+
+class Blog(models.Model):
+    titulo= models.CharField(max_length=60)
+    subtitulo= models.CharField(max_length=70)
+    cuerpo=RichTextField(max_length=3000)
+    autor= models.CharField(max_length=30)
+    fecha= models.CharField(max_length=30)
+    imagen= models.ImageField(upload_to="imagenes")
+    def __str__(self):
+        return f"{self.titulo} - {self.autor}"
+
+class Perfil(models.Model):
+    nombre=models.CharField(max_length=60)
+    apellido=models.CharField(max_length=60)
+    edad=models.IntegerField()
+    email=models.EmailField()
+    descripcion=models.CharField(max_length=60)
+    imagen= models.ImageField(upload_to="avatars")
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ App/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+urlpatterns= [
+
+    path("", inicio, name="Inicio"),
+    path('about/', about, name="About"),
+    path('contact/', contact, name="Contact"),
+    path('pages/', pages, name="Pages" ),
+    path("blogs/", blogs, name="Blogs"),
+
+    path("register/", register, name="register"),
+    path("login/", login_request, name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
+    path("perfil/", perfil, name="perfil"),
+    path("editarPerfil/", editarPerfil, name="editarPerfil"),
+    path("agregarAvatar/", agregarAvatar, name="agregarAvatar"),
+    
+    path("buscar/", buscar, name="buscar"),
+    path("resultados/", resultados, name="resultados"),
+    path("buscarBlog/", buscarBlog, name="buscarBlog"),
+
+    path("crearBlog/", crearBlog, name="crearBlog"),
+    path("leerBlogs/", leerBlogs, name="leerBlogs"),
+    path("editarBlog/<id>", editarBlog, name="editarBlog"),
+    path("eliminarBlog/<id>", eliminarBlog, name="eliminarBlog"),
+    
+
+
+
+
+]
