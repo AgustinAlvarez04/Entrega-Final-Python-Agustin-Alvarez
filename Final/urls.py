93363c8ae8457553febcffe6bc24bac061d7aeb0
AgustinAlvarez04 <121886254+AgustinAlvarez04@users.noreply.github.com>
2023-01-25 02:26:03 -0300

    Add files via upload

--- Final/urls.py
+++ Final/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("App.urls")),
-    
+    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 
